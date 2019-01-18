fc7e5d14201fbb577daf03e679c94ee20bbd0900
bwuhoo <brandon4@asu.edu>
2019-01-17 18:24:41 -0700

    rough layout of what code should look like

--- driverside/src/gui.py
+++ driverside/src/gui.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python
 
 import sys
-import rospy
+# import rospy
 import cv2
-from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow, QAction, qApp
+from import PySide2.QtWidgets import *
 from cv_bridge import CvBridge, CvBridgeError
 from sensor_msgs.msg import Image
 
 
-class GUI(QMainWindow):
+class GUI(QWidget):
     def __init__(self):
         super(GUI, self).__init__()
 
@@ -20,11 +20,17 @@ class GUI(QMainWindow):
 
         # setup file menu and all its items
         file_menu = menubar.addMenu('&File')
+        """
+        restart_item = QAction('&Restart')
+        restart_item.setStatusTip('Relaunch Application')
+        restart_item.triggered.connect(self.restart())
+
+
         quit_item = QAction('&Quit', self)
         quit_item.setStatusTip('Quit Application')
         quit_item.triggered.connect(qApp.quit)
         file_menu.addAction(quit_item)
-
+        """
         # setup debug menu and all its items
         debug_menu = menubar.addMenu('&Debug')
 
@@ -34,15 +40,53 @@ class GUI(QMainWindow):
         # setup status bar
         self.statusBar()
 
-        self.setWindowTitle('ASU MATE 2019')
+        # setup Video object for camera feeds
+        vid = Video()
+
+        self.setWindowTitle('ASU MATE 2019 Video Feed')
         self.showMaximized()
+
+
+class Video(QWidget):
+    def __init__(self):
+        QWidget.__init__(self)
+        self._bridge = CvBridge()
+        self._frame = None
+        self._image = None
+        self._label = QLabel(self)
+        self._pxmp = QPixmap()
+        self._pxmp.fill(QColor(black)
+        self._label.setPixmap(self._image)
+
+    def vid_callback(self, data):
+        try:
+            # convert received ROS Image to cv2 image
+            self._frame = self._bridge.imgmsg_to_cv2(data, "bgr8")
+            print("Frame received")
+
+            # convert cv2 image to QPixmap
+            self._image = QImage(self._frame.data)
+            self._pxmp = QPixmap.fromImage(self._image)
+            print("Frame converted")
+
+            # paint QPixmap as label
+            self._label.setPixmap(self._pxmp)
+            print("Frame painted")
+
+            self.update()
+        # rewrite in Python 2
+        except CvBridgeError as e:
+            print(e)
 
 
 def main():
     app = QApplication([])
+
+    # create node
+    # rospy.init_node('gui')
     gui = GUI()
-    #rospy.init_node('gui')
-    #rospy.spin()
+
+    # rospy.Subscriber("cam_front", Image, gui.prim_vid.vid_callback())
     sys.exit(app.exec_())
 
 
