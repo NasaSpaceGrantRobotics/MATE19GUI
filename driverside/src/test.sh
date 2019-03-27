ab45ff0d0ce2eca94fb3d7ba94b438fea3b06e6d
nasgrds <steven.charles.peterson@gmail.com>
2019-03-26 18:24:44 -0700

    bash file for running and setting up pid

--- driverside/src/test.sh
+++ driverside/src/test.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
-xterm -title "Xbox driver" -e -hold "rmmod xpad; sudo xboxdrv --silent"
+gnome-terminal -x "roscore"     #starts roscore
+wait until
+
+sudo gnome-terminal --working-directory=/home/nasgrds/catkin_ws/MATE19/driverside/src -e "rmmod xpad"
+sudo gnome-terminal --working-directory=/home/nasgrds/catkin_ws/MATE19/driverside/src -e "xboxdrv --silent"     #starts xboxdriver
+wait until
+
+gnome-terminal -x bash -c "rosparam set joy_node/dev /dev/input/js2 && rosrun joy joy_node"     #sets joystick to joy node
+wait until
+
+gnome-terminal -e "roslaunch pid.launch"    #launch pid and input processor
